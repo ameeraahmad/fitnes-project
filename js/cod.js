d17e00cc1de5913bc8e55f63a2f33d967ec433fc
ameeraahmad <ameeraahmad511@gmail.com>
2020-11-15 20:39:56 +0200

    edded new detials

---
+++ js/cod.js
@@ -0,0 +1,13 @@
+$(".toggle").click(function () {
+
+    "use strict";
+
+    $("#_ul").toggleClass('class_mobile');
+});
+
+
+$(window).resize(function () {
+    //    $("#_ul").fadeIn();
+
+});
+
---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*globle console*/
+
+
+var ourinput = document.getElementById("ourinput");
+
+//ourinput.onfocus = function () {
+//    
+//    'use strict';
+//    if (this.placeholder === "Your Name") {
+//        
+//        this.placeholder = '';
+//    }
+//};
+
+//ourinput.onblur = function () {
+//    
+//    'use strict';
+//    if (this.placeholder === "") {
+//        this.placeholder = 'Your Name';
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
